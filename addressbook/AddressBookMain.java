4e9a018664d92e03369030895928756feb88379b
ks0510 <kshaikh051999@gmail.com>
2023-05-13 10:32:31 +0530

    Added java program to implement address book

---
+++ addressbook/AddressBookMain.java
@@ -0,0 +1,21 @@
+/*
+ * This java program creates address book system using collection for person
+ * first name,last name,address,city,zip,phone number and email
+ */
+package addressbook;
+import java.util.*;
+/**
+ * 
+ * @author Kaif
+ *
+ */
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome to Address Book Program");
+
+	}
+}
