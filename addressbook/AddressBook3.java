a93997cdc18f471d81699f29fb2185afb91a62df
ks0510 <126539090+ks0510@users.noreply.github.com>
2023-05-21 08:49:20 +0530

    Added java program to search person by city or state and add it

--- addressbook/AddressBook3.java
+++ addressbook/AddressBook3.java
@@ -12,7 +12,7 @@ import java.io.InputStreamReader;
  *
  */
 
-public class AddressBook3 {
+public class AddressBook_3 {
 
 	public static void main(String[] args) throws IOException {
 		
@@ -24,23 +24,77 @@ public class AddressBook3 {
 		/*
 		 * To create address book with city and person information
 		 */
-		HashMap<String,Person> addressBook1=new HashMap<String,Person>();
+		HashMap<String,Person1> addressBook1=new HashMap<String,Person1>();
 		
 		/*
 		 * To create address book with state and person inforamtion
 		 */
-		HashMap<String,Person> addressBook2=new HashMap<String,Person>();
+		HashMap<String,Person1> addressBook2=new HashMap<String,Person1>();
 		
 		/*
 		 * To take user input for pesron information we are making person object with different information
 		 * 1)First name 2)Second name 3) Address 4)City 5)State 6)Zip code 7)Phone number 8)Email
 		 */
 		System.out.println("enter person1 information");
-		Person p1 = new Person(br.readLine(),br.readLine(),br.readLine(),br.readLine(),br.readLine(), br.read(),br.readLine(),br2.readLine());
+		System.out.println("Enter first name");
+		String firstName = sc.next();
+		System.out.println("Enter last name");
+		String lastName = sc.next();
+		System.out.println("Enter address");
+		String address = sc.next();
+		System.out.println("Enter city");
+		String city = br.readLine();
+		System.out.println("Enter state");
+		String state = br.readLine();
+		System.out.println("Enter zip");
+		int zip = br.read();
+		System.out.println("Enter phone number");
+		String phoneNumber = sc.next();
+		System.out.println("Enter email");
+		String email = sc.next();
+
+		Person1 p1 = new Person1(firstName,lastName,address,city,state,zip,phoneNumber,email);
+		
 		System.out.println("enter person2 information");
-		Person p2 = new Person(br1.readLine(),br1.readLine(),br1.readLine(),br1.readLine(),br1.readLine(), br1.read(),br1.readLine(),br1.readLine());
+		System.out.println("Enter first name");
+		String firstName1 = sc.next();
+		System.out.println("Enter last name");
+		String lastName1 = sc.next();
+		System.out.println("Enter address");
+		String address1 = sc.next();
+		System.out.println("Enter city");
+		String city1 = sc.next();
+		System.out.println("Enter state");
+		String state1 = br2.readLine();
+		System.out.println("Enter zip");
+		int zip1 = br1.read();
+		System.out.println("Enter phone number");
+		String phoneNumber1 = sc.next();
+		System.out.println("Enter email");
+		String email1 = sc.next();
+		
+		Person1 p2 = new Person1(firstName1,lastName1,address1,city1,state1,zip1,phoneNumber1,email1);
+		
 		System.out.println("enter person3 information");
-		Person p3 = new Person(br2.readLine(),br2.readLine(),br2.readLine(),br2.readLine(),br2.readLine(), br2.read(),br2.readLine(),br2.readLine());
+		
+		System.out.println("Enter first name");
+		String firstName2 = sc.next();
+		System.out.println("Enter last name");
+		String lastName2 = sc.next();
+		System.out.println("Enter address");
+		String address2 = sc.next();
+		System.out.println("Enter city");
+		String city2 = sc.next();
+		System.out.println("Enter state");
+		String state2 = sc.next();
+		System.out.println("Enter zip");
+		int zip2 = br2.read();
+		System.out.println("Enter phone number");
+		String phoneNumber2 = sc.next();
+		System.out.println("Enter email");
+		String email2 = sc.next();
+		
+		Person1 p3 =  new Person1(firstName2, lastName2, address2, city2, state2, zip2, phoneNumber2, email2);
 	
 		/*
 		 * Adding to address book with respect to city 
@@ -50,17 +104,10 @@ public class AddressBook3 {
 		addressBook1.put(p2.getCity(), p2);
 		addressBook1.put(p3.getCity(), p3);
 		
-		/*
-		 * To display our addressbook1
-		 * 
-		 */
-		Set<Map.Entry<String,Person>> entries = addressBook1.entrySet();
 		
-		for(Map.Entry<String,Person> p : entries) {
-			System.out.println(p.getKey());
-			Person person = p.getValue();
-			System.out.println(person);
-		}
+		System.out.println("Enter city name");
+		String cityName = sc.next();
+		System.out.println(addressBook1.get(cityName));
 		
 		/*
 		 * Adding to address book with respect to state
@@ -70,17 +117,11 @@ public class AddressBook3 {
 		addressBook2.put(p2.getState(), p2);
 		addressBook2.put(p3.getState(), p3);
 		
-		/*
-		 * To display our addressbook2
-		 * 
-		 */
-		Set<Map.Entry<String,Person>> entries2 = addressBook2.entrySet();
+		System.out.println("Enter state name");
+		String stateName = sc.next();
+		System.out.println(addressBook1.get(stateName));
 		
-		for(Map.Entry<String,Person> p : entries2) {
-			System.out.println(p.getKey());
-			Person person = p.getValue();
-			System.out.println(person);
-		}
+		
 		}
 	}
 
